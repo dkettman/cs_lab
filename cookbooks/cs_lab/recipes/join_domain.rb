9ce3cc6f1209a0781cc386dad36ff5d898cf77b4
Dave Kettmann <david.kettmann@gmail.com>
2023-09-06 07:07:45 -0400

    Added reset of local admin password

--- cookbooks/cs_lab/recipes/join_domain.rb
+++ cookbooks/cs_lab/recipes/join_domain.rb
@@ -34,3 +34,9 @@ end
     property :enabled, 'False'
   end
 end
+
+powershell_script 'Reset Local Administrator Password' do
+  code <<-EOH
+  Get-LocalUser -Name "Administrator" | Set-LocalUser -Password (ConvertTo-SecureString -String #{node['SiteData']['DefaultPassword']} -AsPlainText -Force)
+  EOH
+end
