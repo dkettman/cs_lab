ab8325c5c9e6d43a938978987ea388b8ff3dca4c
Dave Kettmann <david.kettmann@gmail.com>
2023-09-08 07:31:42 -0400

    Moved password reset to new file

--- cookbooks/cs_lab/recipes/join_domain.rb
+++ cookbooks/cs_lab/recipes/join_domain.rb
@@ -38,11 +38,3 @@ end
     property :enabled, 'False'
   end
 end
-
-powershell_script 'Reset Local Administrator Password' do
-  code <<-EOH
-  Get-LocalUser -Name "Administrator" | Set-LocalUser -Password (ConvertTo-SecureString -String #{node['SiteData']['DefaultPassword']} -AsPlainText -Force)
-  EOH
-  #notifies :reboot_now, 'reboot[Restart Computer]', :immediately
-end
-
